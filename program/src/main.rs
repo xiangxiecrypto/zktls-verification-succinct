457e2aa5c4f043d8f206f3be6792e90a3d42a9b4
Xiang Xie <xiexiangiscas@gmail.com>
2025-05-26 19:01:39 +0800

    minor

---
+++ program/src/main.rs
@@ -0,0 +1,23 @@
+//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
+//! number as an output.
+
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
+#![no_main]
+use zktls_att_verification::verification_data::VerifyingDataOpt;
+sp1_zkvm::entrypoint!(main);
+
+// use alloy_sol_types::SolType;
+// use fibonacci_lib::{fibonacci, PublicValuesStruct};
+
+pub fn main() {
+    let verifying_key: String = sp1_zkvm::io::read();
+    let verifying_data: VerifyingDataOpt = sp1_zkvm::io::read();
+
+    let _ = verifying_data.verify(&verifying_key).is_ok();
+
+    sp1_zkvm::io::commit(&verifying_key);
+    sp1_zkvm::io::commit(&verifying_data.get_records());
+}
